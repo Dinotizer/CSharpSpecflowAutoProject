4de76efbb1eeecfe49ea4329c8dd51d6fe82e12d
Sheridan Irvine <sheridan.irvine@outlook.com>
2022-07-25 14:06:52 +0100

    First Test Passing

---
+++ SheridanProject/Pages/PccPage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace SheridanAutoProject.Pages
+{
+    public class PccPage
+    {
+        private readonly IWebDriver _driver;
+        private readonly By _PccLogoLink = By.ClassName("logo__link");
+
+        //public LinkedInPage(IWebDriver driver, IWebElement element)
+        public PccPage(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+        public bool isPccSiteDisplayed()
+        {
+            var isPccSiteDisplayed = _driver.FindElement(_PccLogoLink);
+            return isPccSiteDisplayed.Displayed;
+        }
+    }
+}
