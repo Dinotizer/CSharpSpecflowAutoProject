d7769876a1b596e8337470ba84b56509207ed81c
Sheridan <sheridan.irvine@outlook.com>
2022-07-26 20:58:38 +0100

    Added Test Constant class

---
+++ SheridanProject/Utilities/TestConstants.cs
@@ -0,0 +1,10 @@
+﻿using BoDi;
+using SheridanAutoProject.Pages;
+
+namespace SheridanProject.Utilities
+{
+    public class TestConstants
+    {
+        public const string GoogleWebsite = "http://www.google.co.uk";
+    }
+}
